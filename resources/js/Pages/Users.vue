56e3cb681a039227c9ec2b57c817c769cbda6548
Alexis Selorm Gbeckor-Kove <agbeckor2@gmail.com>
2022-11-19 00:52:08 +0000

    Preserve scroll

--- resources/js/Pages/Users.vue
+++ resources/js/Pages/Users.vue
@@ -2,15 +2,26 @@
     <h1>Users</h1>
     <Nav />
 
+    <div class="mt-[400px]">
+        <p>The current time is {{ time }}.</p>
+    </div>
+    <Link href="/users" class="text-blue-500 hover:underline" preserve-scroll>Refresh</Link>
 
 </template>
 <script lang="ts">
 import Nav from "../Shared/Nav.vue";
 import { defineComponent } from "vue";
+import { Link } from "@inertiajs/inertia-vue3";
 export default defineComponent({
     name: "Users",
     components: {
-        Nav
+        Nav,
+        Link
     },
+    props: {
+        time: {
+            type: String
+        }
+    }
 });
 </script>
