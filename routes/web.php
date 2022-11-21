746f3acb546c42370c1dfa38931a49553a3f35cb
Alexis Selorm Gbeckor-Kove <agbeckor2@gmail.com>
2022-11-21 22:09:58 +0000

    Added comments for user index route

--- routes/web.php
+++ routes/web.php
@@ -23,15 +23,20 @@ Route::get('/users', function () {
 
     return Inertia::render("Users/Index",[
         'users'=>User::query()
+        // When user inputs a search term in the search field, query for a match
         ->when(request()->input('search'),function($query,$search){
             $query->where('name','like',"%{$search}%");
         })
+        // Paginate the user records, 10 per page
         ->paginate(10)
+        // Persist the query params when user changes pages. Assuming a user searched for a name that matches 30 users. The search params should be persisted when the user visits the next or previous page
         ->withQueryString()
+        // Use 'through' to filter out the specific fields you want to send to the frontend. Using 'map' will override the shape of the object return by the query method
         ->through(fn($user)=>[
             'id'=>$user->id,
             'name'=>$user->name,
         ]),
+        // Pass this filters prop to the frontend to persist the search term state
         'filters'=> request() ->only(['search'])
     ]);
 });
