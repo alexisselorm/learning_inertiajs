61da9cf3e53921ab0ad264e642ad947fd805a3fd
Alexis Selorm Gbeckor-Kove <agbeckor2@gmail.com>
2022-11-22 05:32:40 +0000

    Inertia useform helper

--- resources/js/Pages/Users/Create.vue
+++ resources/js/Pages/Users/Create.vue
@@ -9,51 +9,42 @@
             <label class="block mb-2 uppercase font-bold text-xs text-gray-700" for="name">Name</label>
             <input id="name" type="text" name="name" class="border border-gray-400 p-2 w-full" required
                 v-model="form.name">
-            <div v-if="errors.name" v-text="errors.name" class="text-red-500 text-xs mt-1"></div>
+            <div v-if="form.errors.name" v-text="form.errors.name" class="text-red-500 text-xs mt-1"></div>
         </div>
 
         <div class="mb-6">
             <label class="block mb-2 uppercase font-bold text-xs text-gray-700" for="email">Email</label>
             <input id="email" name="email" type="text" class="border border-gray-400 p-2 w-full" required
                 v-model="form.email">
-            <div v-if="errors.email" v-text="errors.email" class="text-red-500 text-xs mt-1"></div>
+            <div v-if="form.errors.email" v-text="form.errors.email" class="text-red-500 text-xs mt-1"></div>
         </div>
         <div class="mb-6">
             <label class="block mb-2 uppercase font-bold text-xs text-gray-700" for="password">Password</label>
             <input id="password" type="password" name="password" class="border border-gray-400 p-2 w-full" required
                 v-model="form.password">
-            <div v-if="errors.password" v-text="errors.password" class="text-red-500 text-xs mt-1"></div>
+            <div v-if="form.errors.password" v-text="form.errors.password" class="text-red-500 text-xs mt-1"></div>
         </div>
         <div class="mb-6">
             <button type="submit" class="bg-blue-400 text-white rounded py-2 px-4 hover:bg-blue-500"
-                :disabled="processing">Submit</button>
+                :disabled="form.processing">Submit</button>
         </div>
 
     </form>
 </template>
 
 <script setup>
-import { reactive, ref } from "vue";
+import { ref } from "vue";
 import { Inertia } from "@inertiajs/inertia"
+import { useForm } from "@inertiajs/inertia-vue3"
 
-// You can choose to use $page.props.errors.<name_of_field> instead of this 'errors' props.
-defineProps({
-    errors: Object
-})
 
-let form = reactive({
+// UseForm gives a reactive object that helps with keeping track of a lot of things. Including whether or not the form has been submitted. And it dynamically toggles the 'disabled' mode of the form's submit method
+let form = useForm({
     name: '',
     email: '',
     password: '',
 });
-// This processing reactive variable keeps track of whether the form has been submitted. If it has, it disables the submit button
-let processing = ref(false);
 let submit = () => {
-    Inertia.post('/users', form,
-        //The third argument to inertia.post. These provide helper functions to dynamically toggle the state of 'processing' on the submit button
-        {
-            onStart: () => { processing.value = true },
-            onFinish: () => { processing.value = false },
-        });
+    form.post('/users')
 }
 </script>
